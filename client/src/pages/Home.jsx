f9c83590ca11e4502d931f05c90343001ed1833d
Raphaelle Olivo <rapholivo@hotmail.fr>
2020-10-12 16:14:09 +0200

    map

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Mapbox from "../components/Mapbox";
 
 const Home = (props) => {
   // Implement react map box here.
   return (
     <div>
+      <Mapbox />
       <h1>MAPBOX MAP HERE</h1>
       <p>On home /</p>
     </div>
